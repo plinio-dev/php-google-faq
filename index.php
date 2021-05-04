8c9abb19b26256fd0d6020c8d3fc9444f77e760b
Tommaso P <tommaso.plinio@gmail.com>
2021-05-04 17:24:35 +0200

    fix

--- index.php
+++ index.php
@@ -29,12 +29,29 @@
 
             'answer' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
 
-            La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
-            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+            La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti\:
+            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
             Google LLC, con sede negli Stati Uniti, per il resto del mondo.
             La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
             Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.'
         ],
+        [
+            'question' => 'Perché il mio account è associato a un paese?',
+
+            'answer' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+
+            La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti\:
+            Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell\'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.
+            Google LLC, con sede negli Stati Uniti, per il resto del mondo.
+            La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.
+            Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.
+            <h4>Stabilire il paese associato al tuo account</h4>
+            Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell\'ultimo anno.
+
+            I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l\'associazione del paese.
+
+            Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l\'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.'
+            ],
 
     ];
 
